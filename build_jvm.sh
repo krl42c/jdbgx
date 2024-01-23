0a068a922bbfc9cd1b5b9a6067eda35da86bdcef
krl42c <karolc9991@gmail.com>
2024-01-23 20:52:32 +0100

    update build_jvm.sh

--- build_jvm.sh
+++ build_jvm.sh
@@ -1,7 +1,8 @@
 # /Users/karol/Library/Java/JavaVirtualMachines/corretto-17.0.8/Contents/Home/include
+# /Users/karol/Library/Java/JavaVirtualMachines/corretto-17.0.8/Contents/Home/include
 
 clang \
-    jdbg.c \
-    -I/Users/karol/Library/Java/JavaVirtualMachines/corretto-17.0.8/Contents/Home/include\
-    -shared -undefined dynamic_lookup -o jdbg_agent.so\
-    -Wall -Wextra -Wpedantic\
+   jdbg_agent.c \
+   -I/Users/karol/Library/Java/JavaVirtualMachines/corretto-17.0.8/Contents/Home/include\
+   -shared -undefined dynamic_lookup -o jdbg_agent.so\
+   -Wall -Wextra -Wpedantic\
